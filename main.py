b2ec156c30ffb720d3dcd74777eaabea20b0f626
Timeness <careless@flash.co>
2025-05-14 18:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 import os
 from config import API_ID, API_HASH, BOT_TOKEN, SESSION_NAME
 
-userbot = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH)
+userbot = Client("userbot_py", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_NAME)
 bot = Client("music_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 pytgcalls = PyTgCalls(userbot)
 
