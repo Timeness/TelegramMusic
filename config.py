568ba72d7e197e95eeb140a10e9992346aa4d6a5
Timeness <careless@flash.co>
2025-05-14 14:25:02 +0530

    done

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 25335325  # Your Telegram API ID
+API_HASH = "9c3e5c9ac118570fad529aabff46fe44"  # Your Telegram API Hash
+BOT_TOKEN = "7555196658:AAE8afMvEmWQ0FwItvo3qvzBrKpZosLCR9k"  # Bot token from BotFather
+SESSION_NAME = "music_session"
+STRING_SESSION = "" # Name for userbot session
+ADMIN_IDS = [123456789]  # Telegram user IDs of admins
+CHAT_ID = -100123456789  # Group/channel ID where the bot will operate
